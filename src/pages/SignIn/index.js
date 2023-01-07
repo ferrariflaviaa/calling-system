7f85877eadba6f41c2913c65cdbcabf9291adfc1
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-01-07 17:31:40 -0300

    feat: creating login page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,39 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.png'
+import { Container, Login, FormLogin } from './styles'
 export default function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Clicou')
+  }
   return (
-    <div>
-      <h1>Página de login</h1>
-    </div>
+    <Container>
+      <Login>
+        <div className="logo">
+          <img src={logo} alt="Sistema logo" />
+        </div>
+        <FormLogin onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input
+            type="email"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="***********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Acessar</button>
+        </FormLogin>
+        <Link to="/register">Criar uma conta</Link>
+      </Login>
+    </Container>
   )
 }
