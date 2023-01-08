213b4d1315b17691ba95533ce04070152fd40288
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-01-08 17:36:01 -0300

    feat: header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import { Container } from './styles.js'
+import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../../context/auth'
+import avatar from '../../assets/avatar.png'
+import { FiHome, FiUser, FiSettings } from 'react-icons/fi'
+
+export default function Header() {
+  const { user } = useContext(AuthContext)
+  return (
+    <div>
+      <div>
+        <img
+          src={user.avatarUrl === null ? avatar : user.avatarUrl}
+          alt="Seu avatar"
+        />
+      </div>
+      <Link to="/dashboard">
+        <FiHome color="#fff" size={24} />
+        Chamados
+      </Link>
+      <Link to="/dashboard">
+        <FiUser color="fff" size={24} />
+        Clientes
+      </Link>
+      <Link to="/dashboard">
+        <FiSettings color="fff" size={24} />
+        Configurações
+      </Link>
+    </div>
+  )
+}
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,13 @@
 import { useContext } from 'react'
 import { AuthContext } from '../../context/auth'
+import Header from '../../components/Header'
 
 export default function Dashboard() {
   const { signOut } = useContext(AuthContext)
 
   return (
     <div>
+      <Header />
       <h1>Página de dashboard</h1>
       <button onClick={() => signOut()}>Fazer logout</button>
     </div>
