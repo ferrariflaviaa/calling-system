f67dae83c863879b117ad2d7b5c175645a665b6c
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-03-11 19:19:53 -0300

    style: layout perfil

--- src/pages/Phofie/index.js
+++ src/pages/Phofie/index.js
@@ -1,18 +1,58 @@
 import Header from '../../components/Header'
 import Title from '../../components/Title'
-import { FiSettings } from 'react-icons/fi'
+import { FiSettings, FiUpload } from 'react-icons/fi'
+import avatar from '../../assets/avatar.png'
 import '../../index.css'
+import { AuthContext } from '../../context/auth'
+import { useContext } from 'react'
+import { useState } from 'react'
+import { Container } from './styles'
 
 export default function Profile() {
+  const { user } = useContext(AuthContext)
+  const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl)
   return (
     <div>
       <Header />
       <div className="content">
         <Title name="Minha conta">
-          <FiSettings size={25}/>
+          <FiSettings size={25} />
         </Title>
+        <Container>
+          <form>
+            <label className="label-avatar">
+              <span>
+                <FiUpload color="#FFF" size={25} />
+              </span>
+              <input type="file" accept="image/*" /> <br />
+              {avatarUrl === null ? (
+                <img
+                  src={avatar}
+                  alt="Foto de perfil"
+                  width={240}
+                  height={240}
+                />
+              ) : (
+                <img
+                  src={avatarUrl}
+                  alt="Foto de perfil"
+                  width={250}
+                  height={250}
+                />
+              )}
+            </label>
+            <label>Nome:</label>
+            <input type="text" placeholder="Seu nome" />
+            <label>Email:</label>
+            <input type="email" placeholder="test@teste.com" disabled />
+
+            <button className='btn' type="submit">Salvar</button>
+          </form>
+        </Container>
+        <Container>
+          <button className="logout-btn">Sair</button>
+        </Container>
       </div>
-      {/* <h1>Página de perfil</h1> */}
     </div>
   )
 }
--- src/pages/Phofie/styles.js
+++ src/pages/Phofie/styles.js
@@ -1,3 +1,85 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-// export const Container
+export const Container = styled.div`
+  display: flex;
+  background-color: #f9f8f8;
+  border-radius: 5px;
+  padding: 0.8em;
+  align-items: center;
+  margin-bottom: 1em;
+
+  .label-avatar {
+    width: 280px;
+    height: 280px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    cursor: pointer;
+
+    input {
+      display: none;
+    }
+    span {
+      z-index: 99;
+      position: absolute;
+      opacity: 0.8;
+      transition: all 0.6s;
+    }
+    span:hover {
+      opacity: 1;
+      transform: scale(1.4);
+    }
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    img {
+      margin-bottom: 1em;
+      border-radius: 50%;
+      object-fit: cover;
+    }
+
+    label {
+      margin-bottom: 0.5em;
+      font-size: 1.4em;
+    }
+
+    input,
+    textarea,
+    select {
+      margin-bottom: 1em;
+      padding: 0.7em;
+      border: 0;
+      border-radius: 5px;
+      max-width: 600px;
+    }
+
+    input: disabled {
+      cursor: not-allowed;
+    }
+
+    button {
+      background-color: #181c2e;
+      color: #f9f8f8;
+      padding: 0.4em;
+      max-width: 600px;
+      border-radius: 6px;
+    }
+    button:hover {
+      background-color: #121212;
+      color: #f9f8f8;
+      opacity: 0.8;
+    }
+  }
+  .logout-btn {
+    padding: 8px 20px;
+    border: 1px solid #121212;
+    border-radius: 5px;
+    font-size: 1.5em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: transparent;
+  }
+`
